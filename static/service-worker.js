5cadfeeab87fbbf0739bde7b99a2e48f1f512181
kishorekumar0814 <kishorekumar1409@gmail.com>
2025-11-17 09:32:15 +0530

    Internal Update

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,15 +1,30 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open("svs-cache").then((cache) => {
-            return cache.addAll(["/"]);
-        })
-    );
+const cacheName = "svs-cache-v1";
+const assetsToCache = [
+  "/",
+  "/static/css/styles.css",
+  "/static/js/main.js",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png",
+  "/static/icons/icon-1024.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+// Activate Service Worker
+self.addEventListener("activate", e => {
+  e.waitUntil(self.clients.claim());
+});
+
+// Fetch cached assets
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
